35b0d452dac3f6bdc5c9518d0f30c63d4c738096
yoonwooiOS <yoonwoo4429@kakao.com>
2024-09-08 22:02:06 +0900

    Initial Commit

---
+++ CryptoMarket/CryptoMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoMarketApp.swift
+//  CryptoMarket
+//
+//  Created by 김윤우 on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
